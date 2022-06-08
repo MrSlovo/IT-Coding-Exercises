b4827411ca38fdd319ce0ee8770de2dea5e6f587
Nsovo Ngobeni <97952201+MrSlovo@users.noreply.github.com>
2022-06-08 14:16:16 +0200

    completed the first class

---
+++ Gr 12/Past Papers/2011 Prelim/src/Votes.java
@@ -0,0 +1,36 @@
+public class Votes {
+  private int wardID;
+  private String districtID;
+  private int total;
+  private int resultA;
+  private int resultB;
+
+  public Votes(String wardID, String districtID, int total, int resultA, int resultB) {
+    setWardID(wardID);
+    this.districtID = districtID;
+    this.total = total;
+    this.resultA = resultA;
+    this.resultB = resultB;
+  }
+
+  public void setWardID(String wardID) {
+    int num = Integer.parseInt(wardID.substring(wardID.length() - 3));
+    this.wardID = num;
+  }
+  
+  public int calcA() {
+    int voteShare = Math.round((resultA) / total * 100);
+    return voteShare;
+  }
+  
+  public int calcB() {
+    int voteShare = Math.round((resultB) / total * 100);
+    return voteShare;
+  }
+
+  @Override
+  public String toString() {
+    return "wardID: " + wardID + "\tdistrictID: " + districtID + "\ttotal: " + total + "\tresultA: " + resultA + "\tresultB: " + resultB;
+  }
+  
+}
